d395a128724a53e735a4d2c6c70c7b3d0a516b44
poornimapo <167285037+poornimapo@users.noreply.github.com>
2024-12-30 21:13:34 +0530

    Add files via upload

---
+++ poorni.py
@@ -0,0 +1,14 @@
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+c=int(input("enter a number"))
+d=int(input("enter a number"))
+e=(a+b)*c/d
+print("value of (a+b)*c/d is ",e)
+e=a+(b*c)/d
+print("value of a+(b*c)/d is ",e)
+e=a*(b-c)+d
+print("value of a*(b-c)+d is ",e)
+e=2**3**2
+print("value of 2**3**2 is ",e)
+e=5*2//3
+print("value of 5*2//3 is ",e)
